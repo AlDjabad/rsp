37214d8eb440ade08f75d64260f80f0192e5ffca
AlDjabad <sergei.adonev@yahoo.com>
2024-07-23 00:42:01 +0200

    Project setup

---
+++ src/test/java/com/charonsroad/rsp/RspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charonsroad.rsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
